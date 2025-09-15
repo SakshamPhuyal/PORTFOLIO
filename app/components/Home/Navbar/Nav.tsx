d629685c3bf85fdca9d1839e44c7e6581f48a236
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-09-15 09:18:20 +0545

    Refactor layout and styling; update project metadata and add new components

---
+++ app/components/Home/Navbar/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='transition-all duration-200 h-[12vh] z-[1000] fixed w-full text-white'>
+      Nav
+    </div>
+  )
+}
+
+export default Nav

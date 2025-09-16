002274cd0be84239ee5051de98ba5a7c0656c609
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-09-16 12:10:30 +0545

    navbar

--- app/components/Home/Navbar/Nav.tsx
+++ app/components/Home/Navbar/Nav.tsx
@@ -15,7 +15,7 @@ window.addEventListener("scroll",handler);
 return ()=>window.removeEventListener("scroll",handler);  
 },[]);
   return (
-    <div className='transition-all duration-200 h-[12vh] z-[1000] fixed w-full text-white'>
+    <div className={`transition-all ${navbg?'bg-[#0f142ed9] shadow-md':"fixed"} duration-200 h-[12vh] z-[1000] fixed w-full text-white`}>
       <div className='flex items-center h-full justify-between w-[90%] mx-auto'>
         <div className='flex items-center space-x-2'>
           <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col'>

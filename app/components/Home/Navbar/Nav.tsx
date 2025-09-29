f3f5d72ece7d54e16bfcc6449b60cdf94c83a91a
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-09-29 14:57:05 +0545

    routing

--- app/components/Home/Navbar/Nav.tsx
+++ app/components/Home/Navbar/Nav.tsx
@@ -36,13 +36,14 @@ const Nav = () => {
       <div className='flex items-center h-full justify-between w-[90%] mx-auto'>
         
         {/* Logo */}
-        <div className='flex items-center space-x-2'>
-          <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col'>
-            <FaCode className='w-5 h-5 text-black' />
-          </div>
-          <h1 className='text-2xl text-white font-bold'>SAKSHAM</h1>
-        </div>
-
+       <div className='flex items-center space-x-2'>
+  <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col'>
+    <FaCode className='w-5 h-5 text-black' />
+  </div>
+  <Link href="/" className="text-2xl text-white font-bold hover:text-cyan-400 transition">
+    SAKSHAM
+  </Link>
+</div>
         {/* Navigation Links */}
         <div className='flex items-center space-x-10'>
           {Navlinks.map((link) => {
